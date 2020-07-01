0ad6d09ea1d935cbbe70007381dfac4f21741539
christina.gorina <christina.gorina@yandex.ru>
2020-07-01 18:03:42 +0300

    Populate data base

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,10 +1,19 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
 
 public class MainApp {
     private static final Logger log = LoggerFactory.getLogger(MainApp.class);
     public static void main(String[] args) {
         log.info("MainApp");
         System.out.println("Start");
+
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-db.xml")) {
+            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        }
     }
 }
+
---
+++ src/main/java/org/christinagorina/model/Dish.java
@@ -0,0 +1,4 @@
+package org.christinagorina.model;
+
+public class Dish {
+}
